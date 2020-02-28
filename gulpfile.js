f47e4638328050896b8e31cb097379b047888085
malcolmu <mjjf86@gmail.com>
2020-02-28 13:30:16 +0000

    some package.json changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,45 @@
 /* eslint-disable */
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-var imagemin = require('gulp-imagemin');
-var cleanCSS = require('gulp-clean-css');
-var rename = require('gulp-rename');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var sourcemaps = require("gulp-sourcemaps");
+var imagemin = require("gulp-imagemin");
+var cleanCSS = require("gulp-clean-css");
+var rename = require("gulp-rename");
+var browserSync = require("browser-sync").create();
 
 var paths = {
   styles: {
-    src: './scss/**/*.scss',
+    src: "./scss/**/*.scss"
   },
   scripts: {
-    src: './js/**/*.js',
-  },
+    src: "./js/**/*.js"
+  }
 };
+
+// Static server
+gulp.task("serve", [styles], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
 
 function styles() {
   return gulp
     .src(paths.styles.src)
     .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
     .pipe(
       autoprefixer({
-        cascade: false,
-      }),
+        cascade: false
+      })
     )
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./css'));
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("./css"));
 }
 
 /*
@@ -39,35 +49,35 @@ function styles() {
 
 function minifyCSS() {
   return gulp
-    .src('css/*.css')
+    .src("css/*.css")
     .pipe(
       cleanCSS({ debug: true }, function(details) {
-        console.log('=========================================');
-        console.log(details.name + ': ' + details.stats.originalSize);
-        console.log(details.name + ': ' + details.stats.minifiedSize);
-        console.log('=========================================');
-      }),
+        console.log("=========================================");
+        console.log(details.name + ": " + details.stats.originalSize);
+        console.log(details.name + ": " + details.stats.minifiedSize);
+        console.log("=========================================");
+      })
     )
     .pipe(
       rename({
-        suffix: '.min',
-      }),
+        suffix: ".min"
+      })
     )
-    .pipe(gulp.dest('css/min'));
+    .pipe(gulp.dest("css/min"));
 }
 
 // Optimizing Images
 function optimiseImages() {
   return (
     gulp
-      .src('./img/**/*.+(png|jpg|jpeg|gif|svg)')
+      .src("./img/**/*.+(png|jpg|jpeg|gif|svg)")
       // Caching images that ran through imagemin
       .pipe(
         imagemin({
-          interlaced: true,
-        }),
+          interlaced: true
+        })
       )
-      .pipe(gulp.dest('./img'))
+      .pipe(gulp.dest("./img"))
   );
 }
 
